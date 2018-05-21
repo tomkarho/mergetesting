34d156a1798fe10dd344ab93d8483ec62f8911b5
toka <tommi.kaura-aho@protacon.com>
2018-05-21 12:44:24 +0300

    Create environment

--- deployment/createEnvironment.ps1
+++ deployment/createEnvironment.ps1
@@ -8,4 +8,20 @@ Param(
     [Parameter()]$spTenant = ""
 )
 
+$ErrorActionPreference = "stop"
+$location = "westeurope"
+$servicePlan = "S1"
+
 Write-Host "Creating environment..."
+
+az login --service-principal -u $spUser --password $spPswd --tenant $spTenant
+
+az group create --location $location --name $resourceGroup --tags EnvironmentType=$envType
+
+az appservice plan create --name $webApp --resource-group $resourceGroup --sku $servicePlan
+az webapp create --name $webApp --resource-group $resourceGroup --plan $webApp
+
+az webapp config set --name $webApp --resource-group $resourceGroup --always-on true --php-version "Off"
+az webapp log config --name $webApp --resource-group $resourceGroup --application-logging true --detailed-error-messages true --failed-request-tracing true --level verbose
+
+az webapp deployment source config-local-git --name $webApp --resource-group $resourceGroup
